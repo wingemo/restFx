371d597d03c005d247dba81a3e754646d7b60c88
Wingemo <philip.wingemo@outlook.com>
2021-08-25 10:11:40 +0200

    Update controller.js

--- src/controller.js
+++ src/controller.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
     marg = 0;
     var orderIDarray = [];
     $("#add").click(function() {
-
         var inArray = jQuery.inArray($("#OrderID").val(), orderIDarray);
-
         if (inArray == -1) {
-
             $("#orders").append('<tr class="header tableopen" id="' + $("#OrderID").val() + '" ><td>' + $("#OrderID").val() + ' </td><td>' + $("#Day").val() + ' </td><td id="' + $("#OrderID").val() + '" class="price">' + $("#Amount").val
 
                 () + ' </td><td>' + $("#Currency").val() + ' </td><td><button type="button" id="' + $("#OrderID").val() + '" style="width:100%;" class="send btn btn-primary  btn-block">Trade</button></td></tr>   <tr data-for="' + $("#OrderID").val() + '" style="background-color:#FBF2EA; border-width: 0;display:none; width: 100px"><td style="border: none;"></td>  <td style="border: none;">  <input type="text" placeholder="Currency Pair"  class="form-control pair" aria-label="Small" disabled aria-describedby="inputGroup-sizing-sm" value="' + $("#Currency").val() + 'SEK" ></td><td style="border: none;">  <input type="text" placeholder="Settlement Date" class="date form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value="' + $("#Day").val() + '" id="' + $("#OrderID").val() + '"></td><td style="border: none;">  <input id="' + $("#OrderID").val() + '"   placeholder="Side (BUY/SELL)" type="text" class="form-control side"  aria-label="Small" aria-describedby="inputGroup-sizing-sm" ></td><td  style="border: none;"><div style="height: 70px; overflow:hidden;"><input id="' + $("#OrderID").val() + '" type="text"  class="form-control total" aria-label="Small" placeholder="Total trade amount" aria-describedby="inputGroup-sizing-sm"></div></td></tr><tr data-for="' + $("#OrderID").val() + '" style="background-color:#FBF2EA; border-width: 0;display:none; width: 100px"><td></td><td></td><td></td><td><select class="form-select tenor" id="' + $("#OrderID").val() + '"> <option selected value="2">Tenor</option><option value="1">1M</option><option value="2">2M</option><option value="3">3M</option><option value="4">4M</option><option value="5">5M</option><option value="6">6M</option><option value="7">7M</option><option value="8">8M</option><option value="9">9M</option></select></td><td><select class="form-select select_amount" id="' + $("#OrderID").val() + '"> <option selected>Trade amount %</option> <option value="0.10">10%</option><option value="0.20">20%</option><option value="0.30">30%</option><option value="0.40">40%</option><option value="0.50">50%</option><option value="0.60">60%</option><option value="0.70">70%</option><option value="0.80">80%</option><option value="0.90">90%</option></select></td></tr>');
@@ -14,19 +11,12 @@ $(document).ready(function() {
             $("#" + $("#OrderID").val() + ".total").val($("#Amount").val());
 
             $("#" + $("#OrderID").val() + ".side").val("SELL");
-
         } else {
-
             $("body").prepend('<div id="hover"  aria-live="polite"  data-autohide="false"  aria-atomic="true" style="position: relative; min-height: px; z-index: 1000"> <div class="toast" data-autohide="false"  style="margin-right: 40px;position: absolute; top:750px; right: 0;"><div class="toast-header bg-danger" style="color: white"> <strong class="mr-auto">Error Message&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong> </div>  <div class="toast-body">OrderID must be unique</div>  </div></div></div>');
-
             $('.toast').toast('show');
-
         }
-
         let tmp = $("#OrderID").val();
-
         orderIDarray.push(tmp);
-
     });
     $("body").on("click", ".tenor", function(e) {
         let tenor = $("#" + this.id + ".tenor").val();
