5c062013feabef7c3e2cc8c691ffd55d74bc0d1b
Wingemo <philip.wingemo@outlook.com>
2021-08-25 20:00:16 +0200

    Update model.js

--- src/models/model.js
+++ src/models/model.js
@@ -10,6 +10,7 @@
 - Data comes from the controller
 */
 function model(obj, sum, date, side) {
+	let tmp;
 	var request = $.ajax({
 		method: "POST",
 		url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/adapter/adapter.php",
@@ -25,6 +26,7 @@ function model(obj, sum, date, side) {
 		}
 	});
 	request.done(function( msg ) {
-	  return JSON.parse(msg);
+	  temp = JSON.parse(msg);
 	});
+	return tmp;
 };
