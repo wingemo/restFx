a50a008c58ffc754431483c9a145977063da1f0b
Wingemo <philip.wingemo@outlook.com>
2021-08-25 13:52:42 +0200

    Update make-request.js

--- scripts/make-request.js
+++ scripts/make-request.js
@@ -1,1 +1,19 @@
 
+    $.ajax({
+            method: "POST",
+            url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
+            data: {
+                amount: sum,
+                amountCurrency: 1000,
+                currencyPair: "EURSEK",
+                externalId: 212112,
+                meansOfPayment: "HEDGE",
+                settlementDate: "2021-08-30",
+                side: "BUY,
+                timeout: 11000
+            }
+        })
+        .done(function(msg) {
+             console.log(JSON.parse(msg));
+        })
+
