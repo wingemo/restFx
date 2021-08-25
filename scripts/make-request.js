c90eb2d0925dfaeddd86df8d94a48484321da457
Wingemo <philip.wingemo@outlook.com>
2021-08-25 13:57:18 +0200

    Delete make-request.js

--- scripts/make-request.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-         method: "GET",
-         url: "https://psd2.api.swedbank.com/partner/sandbox/v1/fx/market-order/orders",
-    .done(function(msg) {
-        console.log(JSON.parse(msg));
-    })
-});
