fd2bfef94191360c014f6414f7e64fc77c8a288c
Wingemo <philip.wingemo@outlook.com>
2021-08-25 13:57:08 +0200

    Update make-request.js

--- scripts/make-request.js
+++ scripts/make-request.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.ajax({
-            method: "GET",
-            url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
-        .done(function(msg) {
-            console.log(JSON.parse(msg));
-        })
+         method: "GET",
+         url: "https://psd2.api.swedbank.com/partner/sandbox/v1/fx/market-order/orders",
+    .done(function(msg) {
+        console.log(JSON.parse(msg));
+    })
 });
