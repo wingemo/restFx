bb589ce51db84c0c19e059f813c1d4e7caa89b39
Wingemo <philip.wingemo@outlook.com>
2021-08-25 11:05:10 +0200

    Update model.js

--- src/models/model.js
+++ src/models/model.js
@@ -1,4 +1,4 @@
-function model(obj, sum, date) {
+function model(obj, sum, date, side) {
     $.ajax({
             method: "POST",
             url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
