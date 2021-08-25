7ebc240cafe97b2d44c69af34541dfc346479879
Wingemo <philip.wingemo@outlook.com>
2021-08-25 10:49:59 +0200

    Update model.js

--- src/models/model.js
+++ src/models/model.js
@@ -1,1 +1,18 @@
-hello
+$.ajax({
+        method: "POST",
+        url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
+        data: {
+            amount: sum,
+            amountCurrency: obj[3],
+            currencyPair: obj[3] + "SEK",
+            externalId: obj[0],
+            meansOfPayment: "HEDGE",
+            settlementDate: date,
+            side: side,
+            timeout: 11000
+        }
+    })
+    .done(function(msg) {
+        let obj = JSON.parse(msg);
+        return obj;
+    })
