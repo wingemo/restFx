5c65d43ad293e27c280043043cb83b50e1074830
Wingemo <philip.wingemo@outlook.com>
2021-08-25 13:02:09 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['url'] = "localhost";
-$config['app-id'] = "l77541fe6427284935b875fca91eb87cdd";
+$config['url'] = "https://psd2.api.swedbank.com/partner/sandbox/v1/fx/market-order/orders?";
+$config['app-id'] = "app-id=l77541fe6427284935b875fca91eb87cdd";
 
 ?>
