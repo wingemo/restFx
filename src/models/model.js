5c00e06edbad637f9bb48c9d747a16d7a49b2b95
Wingemo <philip.wingemo@outlook.com>
2021-08-25 14:01:06 +0200

    Update model.js

--- src/models/model.js
+++ src/models/model.js
@@ -1,23 +1,20 @@
-jQuery.fn.model = function(obj, sum, date, side) {
-     let tmp;
-    $.ajax({
-            method: "POST",
-            url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
-            data: {
-                amount: sum,
-                amountCurrency: obj[3],
-                currencyPair: obj[3] + "SEK",
-                externalId: obj[0],
-                meansOfPayment: "HEDGE",
-                settlementDate: date,
-                side: side,
-                timeout: 11000
-            }
-        })
-        .done(function(msg) {
-            tmp = JSON.parse(msg);
-             console.log(tmp);
-        })
-    console.log(tmp);
-    return tmp;
+jQuery.fn.model = function (obj, sum, date, side) {
+	$.ajax({
+		method: "POST",
+		url: "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/middleware/adapter.php",
+		data: {
+			amount: sum,
+			amountCurrency: obj[3],
+			currencyPair: obj[3] + "SEK",
+			externalId: obj[0],
+			meansOfPayment: "HEDGE",
+			settlementDate: date,
+			side: side,
+			timeout: 11000
+		}
+	})
+	.done(function (msg) {
+		let tmp = JSON.parse(msg);
+		console.log(tmp);
+	})
 };
