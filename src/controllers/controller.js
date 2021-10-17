a68fd5de7f044ae8543d2f4a2b106ec72517c73b
Wingemo <philip.wingemo@outlook.com>
2021-10-17 18:44:05 +0200

    Update controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
    $("#Day").val(today);
    $("#Amount").val(Math.floor(Math.random() * 1001));
    $("#Currency").val("EUR");
-   $("#add").click();
+   $("#add").trigger('click');
    console.log("hello");
    /*
    - Adds a new order to a table on the page 
