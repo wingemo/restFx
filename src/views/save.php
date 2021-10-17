525ae6bd6525ff0b2ffcee0a8fcea2b7aaecc595
Wingemo <philip.wingemo@outlook.com>
2021-10-17 23:08:56 +0200

    Update save.php

--- src/views/save.php
+++ src/views/save.php
@@ -1,8 +1,8 @@
 <?php
 $stringa=$_POST['data'];
 $ourFileName = "swedbank.html";
-if (!file_exists($ourFileName)) {
-  print 'File not found';
+if (file_exists($ourFileName)) {
+  print 'lol';
 }
 $ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
 fwrite($ourFileHandle,$stringa);
