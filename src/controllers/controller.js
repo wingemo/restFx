356e80f2db024d369aaa4d6013574172680d2fe9
Wingemo <philip.wingemo@outlook.com>
2021-10-17 23:07:46 +0200

    Update controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
              $(e.target).html(prompt("Text", ""));
             if (confirm('Save?')) {
                var newData= document.documentElement.innerHTML;
-               $.post("http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/adapter/save.php", { data: newData} )
+               $.post("http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/src/views/save.php", { data: newData} )
                  .done(function( data ) {
                    alert( "Data Loaded: " + data );
                  });
