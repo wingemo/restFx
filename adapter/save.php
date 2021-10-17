af1d3dd71c09398f93d808b9c74df8a031a347f6
Wingemo <philip.wingemo@outlook.com>
2021-10-17 23:06:00 +0200

    Update save.php

--- adapter/save.php
+++ adapter/save.php
@@ -1,6 +1,6 @@
 <?php
 $stringa=$_POST['data'];
-$ourFileName = "http://3.67.145.166/partner/sandbox/v1/fx/market-order/orders/src/views/swedbank.html";
+$ourFileName = "/htdocs/partner/sandbox/v1/fx/market-order/orders/src/views/swedbank.html";
 $ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
 fwrite($ourFileHandle,$stringa);
 fclose($ourFileHandle);
