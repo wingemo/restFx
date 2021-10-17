f2a6cd739f098a64fe800447620b384cbdcdde48
Wingemo <philip.wingemo@outlook.com>
2021-10-17 23:41:30 +0200

    Update save.php

--- src/views/save.php
+++ src/views/save.php
@@ -1,6 +1,6 @@
 <?php
 $stringa=$_POST['data'];
 $ourFileName = "swedbank.html";
-$ourFileHandle = fopen($ourFileName, 'w');
+$ourFileHandle = fopen($ourFileName, 'w') or die("Unable to open file!");
 fwrite($ourFileHandle,$stringa);
 fclose($ourFileHandle);
